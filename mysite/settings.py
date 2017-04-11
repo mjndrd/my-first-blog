de204640de5d09fa94d8b361e8d1821db6a4007b
Odoardo Majnardi <odoardo.majnardi@gmail.com>
2017-04-11 11:29:05 +0200

    modifiche a settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = '(ix$5!jd-dom7c^1y6xf%cc9l#6jdyqlalfvd))9hj!3ts_r2v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+	"localhost"
+]
 
 
 # Application definition
@@ -119,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
